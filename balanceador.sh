6a7348b23f5cb2bf5079e943ecccf9bfbfa003fe
knyu07 <pventura9839@gmail.com>
2020-11-13 23:03:57 +0100

    terminada

--- balanceador.sh
+++ balanceador.sh
@@ -10,10 +10,6 @@ apt upgrade -y
 # APACHE
 apt install apache2 -y
 
-# Descargamos y copiamos el archivo de configuración de Apache
-git clone https://github.com/knyu07/iaw-practica-05
-cp /home/ubuntu/iaw-practica-05/000-default.conf /etc/apache2/sites-available/
-
 #Activamos los módulos
 a2enmod proxy
 a2enmod proxy_http
@@ -25,6 +21,10 @@ a2enmod proxy_balancer
 a2enmod proxy_connect
 a2enmod proxy_html
 a2enmod lbmethod_byrequests
+
+# Descargamos y copiamos el archivo de configuración de Apache
+git clone https://github.com/knyu07/iaw-practica-05
+cp /home/ubuntu/iaw-practica-05/000-default.conf /etc/apache2/sites-available/
 
 #Reiniciamos
 systemctl restart apache2
